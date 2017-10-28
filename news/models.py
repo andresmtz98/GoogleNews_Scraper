5d2fd2a329854852a9be63124647ba7f038e9cc8
andresmtz98 <andresfmtz08@gmail.com>
2017-10-28 12:46:00 -0500

    Update models.py

--- news/models.py
+++ news/models.py
@@ -6,10 +6,10 @@ class News(models.Model):
     title = models.TextField(max_length=10000000000000)
     description = models.TextField(max_length=10000000000000000000000)
     url = models.URLField()
-    date = models.CharField(max_length=255)
-    url_image = models.TextField(max_length=500)
-    source = models.CharField(max_length=255)
-    latest_updated = models.CharField(max_length=255) #Ultima fecha en que se actualizó
+    date = models.CharField(max_length=255, null=True)
+    url_image = models.TextField(max_length=500, null=True)
+    source = models.CharField(max_length=255, null=True)
+    latest_updated = models.CharField(max_length=255, null=True) #Ultima fecha en que se actualizó
     
 
     def __str__(self):
