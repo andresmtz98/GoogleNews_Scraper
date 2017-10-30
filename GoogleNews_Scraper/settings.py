95c1368e914f9a316d636572bace039728db1df5
andresmtz98 <andresfmtz08@gmail.com>
2017-10-29 19:18:03 -0500

    Update settings.py

--- GoogleNews_Scraper/settings.py
+++ GoogleNews_Scraper/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'GoogleNews_Scraper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'google_news_scrapper', 
+        'USER': 'admin_gnews',
+        'PASSWORD': 'prestige'#,
+        #'PORT': '5434'
     }
 }
 
